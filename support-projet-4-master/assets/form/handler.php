7ba07a63c9b02360d47368508cc42c2cce1e5113
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-23 18:20:55 +0300

    Update handler.php

--- support-projet-4-master/assets/form/handler.php
+++ support-projet-4-master/assets/form/handler.php
@@ -17,6 +17,6 @@ $validator->field('message')->maxLength(6000);
 
 
 // ENTRER E-MAIL ICI
-$pp->sendEmailTo("admb34@yandex.ru"); 
+$pp->sendEmailTo("adelbouatta@hotmail.fr"); 
 
 echo $pp->process($_POST);
